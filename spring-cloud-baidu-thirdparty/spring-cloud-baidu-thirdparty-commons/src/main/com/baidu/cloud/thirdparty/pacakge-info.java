7281508a7a02c27288ff1fdf0cd25c85feb162dd
tully <tullyliu@gmail.com>
2022-12-02 15:20:16 +0800

    Update pacakge-info.java

--- spring-cloud-baidu-thirdparty/spring-cloud-baidu-thirdparty-commons/src/main/com/baidu/cloud/thirdparty/pacakge-info.java
+++ spring-cloud-baidu-thirdparty/spring-cloud-baidu-thirdparty-commons/src/main/com/baidu/cloud/thirdparty/pacakge-info.java
@@ -2,5 +2,12 @@
  * 
  * All the thirdparty jar will change package name to com.baidu.cloud.thirdparty.
  * 
+ * @author cpd
+ * 
  */
 package com.baidu.cloud.thirdparty;
+
+class PkgConst{
+    static final String PACAKGE_CONST="com.baidu.cloud.thirdparty";
+}
+
